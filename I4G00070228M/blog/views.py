b9d0cb8e4b7ae606bd1223428b60768cf002b48f
Orioha Chikezirim <oriohac@gmail.com>
2022-06-23 17:19:55 +0100

    create Django blog apps and implement the CRUD using the Model, View and Template.

---
+++ I4G00070228M/blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+
+
+
+from .models import Post
+# Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
